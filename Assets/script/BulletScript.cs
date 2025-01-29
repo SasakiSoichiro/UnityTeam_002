0c3a9678976c09d47ada9dd182f25b969b93d43d
ShouyaChikada <241313@st.yoshida-g.ac.jp>
2025-01-29 10:13:11 +0900

    a

--- Assets/script/BulletScript.cs
+++ Assets/script/BulletScript.cs
@@ -4,18 +4,38 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-   public GameObject Sphere;
-   public float speed = 20f; // 弾のスピード
-   void Start() 
-   { // 弾の移動方向を設定
-        Rigidbody rb = Sphere.GetComponent<Rigidbody>(); 
-        rb.velocity = transform.forward * speed; 
-   } 
-   void OnCollisionEnter(Collision collision) 
-   { // ターゲットに当たったら弾を消す
-        if (collision.gameObject.CompareTag("Target")) 
-        { 
-            Destroy(Sphere); 
-        } 
-   }
+    public GameObject Sphere;
+    public GameObject Bullet;
+    public Transform firePoint;     // 弾が発射される位置
+    public float speed = 20f; // 弾のスピード
+    void Start()
+    { // 弾の移動方向を設定
+        Rigidbody rb = Bullet.GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+    }
+    void Update()
+    {
+        // マウス左クリックで弾を発射
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    { // ターゲットに当たったら弾を消す
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            Destroy(Sphere);
+        }
+    }
+
+
+    void Shoot()
+    {
+        // 弾を生成
+        GameObject bullet = Instantiate(Bullet, firePoint.position, firePoint.rotation);
+
+        // Rigidbodyで弾を飛ばす
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+    }
 }
